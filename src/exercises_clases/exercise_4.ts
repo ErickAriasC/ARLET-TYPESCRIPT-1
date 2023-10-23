059055b308f515a5b2709400d7fdf9cce8f63aea
Erick Arias <fernandaarias328@gmail.com>
2023-10-23 15:12:55 -0500

    se inicia el ejercico 4

---
+++ src/exercises_clases/exercise_4.ts
@@ -0,0 +1,19 @@
+// Crear una clase FiguraGeometrica y subclases Triángulo, 
+// Círculo y Cuadrado. Implementar el método area() en
+// cada subclase. Crear objetos y calcular sus áreas
+
+class FiguraGeometrica {
+
+}
+
+class Triangulo{
+
+}
+
+class Circulo{
+    
+}
+
+class Cuadrado{
+    
+}
